9478848912ca00c31a07cc8c7c31f9ad51511812
Filipe Brandão <fdabrandao@gmail.com>
2024-08-05 19:11:29 +0100

    Switch from superapp to st.Page

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,33 +1,16 @@
 import streamlit as st
 import platform
 
-INFO_HEADER = """
-[AMPL](https://ampl.com) is the most powerful and intuitive tool for developing and deploying
-complex optimization solutions in business & scientific applications.
-"""
 
-INFO_FOOTER = """
-- [Documentation](https://dev.ampl.com/)
-- [Support Forum](https://discuss.ampl.com/)
-- [Model Colaboratory](https://ampl.com/colab/)
-
-Follow us on [Twitter](https://twitter.com/AMPLopt) and [LinkedIn](https://www.linkedin.com/company/ampl) to get the latest updates from the dev team!
-"""
+def badge(url_path=""):
+    if platform.system() != "Linux":
+        return ""
+    # return f"[![Hits](https://h.ampl.com/https://amplopt.streamlit.app/{url_path})](https://github.com/fdabrandao/amplopt.streamlit.app)"
+    return f"<img src='https://h.ampl.com/https://amplopt.streamlit.app/{url_path}' style='float:right;'>"
 
 
-def common_header():
+def common_header(url_path):
     # Logo
     st.image("https://portal.ampl.com/dl/ads/python_ecosystem_badge.png")
 
-    # Sidebar
-    st.sidebar.header("About")
-    st.sidebar.markdown(INFO_HEADER)
-
-    st.sidebar.header("Resources")
-    st.sidebar.markdown(INFO_FOOTER)
-
-
-def badge(app=""):
-    if platform.system() != "Linux":
-        return ""
-    return f"[![Hits](https://h.ampl.com/https://amplopt.streamlit.app/{app})](https://github.com/fdabrandao/amplopt.streamlit.app)"
+    st.write(badge(url_path), unsafe_allow_html=True)
--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,19 @@ import os
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
-from apps import badge, python
+from apps import common_header
+from apps import (
+    bistro_game,
+    global_optimization,
+    batch_process,
+    facility_location,
+    aircrew_training_scheduling,
+    risk_return,
+    nqueens,
+    tips,
+    python,
+    reformulation_explorer,
+)
 
 st.set_page_config(
     page_title="AMPL on Streamlit Cloud",
@@ -26,39 +38,79 @@ def activate_license():
 activate_license()
 
 
-# Banner
-st.image("https://portal.ampl.com/dl/ads/python_ecosystem_badge.png")
+def home():
+    st.write("# Welcome to AMPL on Streamlit! 👋")
 
-st.write("# Welcome to AMPL on Streamlit! 👋")
+    st.markdown(
+        """
+        [AMPL](https://ampl.com) is the most powerful and intuitive tool for developing and deploying
+        complex optimization solutions in business & scientific applications.
+        """
+    )
 
-st.sidebar.success("Select an app above.")
+    st.success(
+        "**👈 Select an app from the sidebar** to see some examples of what you can do with AMPL on Streamlit!"
+    )
 
-st.markdown(
+    st.markdown(
+        """
+        - [📖 Documentation](https://dev.ampl.com/)
+        - [💬 Support Forum](https://discuss.ampl.com/)
+        - [🧑‍🤝‍🧑 Model Colaboratory](https://ampl.com/colab/)
+
+        Follow us on [LinkedIn](https://www.linkedin.com/company/ampl) and [Twitter](https://twitter.com/AMPLopt) to get the latest updates from the dev team!
+        """
+    )
+
+    python.main()
+
+    st.markdown(
+        """
+    [[App Source Code on GitHub](https://github.com/fdabrandao/amplopt.streamlit.app/tree/master/apps/tips)]
+    [[Build your own App with AMPL & Streamlit](https://dev.ampl.com/ampl/python/streamlit.html)]
     """
-[AMPL](https://ampl.com) is the most powerful and intuitive tool for developing and deploying
-complex optimization solutions in business & scientific applications.
-            
-**👈 Select an app from the sidebar** to see some examples of what you can do with AMPL on Streamlit!"""
-)
+    )
 
-links = {}
-for fname in os.listdir("pages"):
-    if fname.endswith(".py"):
-        fname = fname[:-3]
-        page_number = int(fname[: fname.find("_")])
-        fname = fname[fname.find("_") + 1 :]
-        url = fname[fname.find("_") + 1 :]
-        label = fname.replace("_", " ")
-        links[page_number] = f"- [{label}]({url})"
-for page_number in sorted(links):
-    st.markdown(links[page_number])
 
-python.main()
+def app_page(app, icon, title):
+    url_path = title.replace(" ", "_")
+    if title == "Home":
+        url_path = ""
 
-st.markdown(badge(""))
-st.markdown(
-    """
-[[App Source Code on GitHub](https://github.com/fdabrandao/amplopt.streamlit.app/tree/master/apps/tips)]
-[[Build your own App with AMPL & Streamlit](https://dev.ampl.com/ampl/python/streamlit.html)]
-"""
-)
+    def page():
+        common_header(url_path)
+        app()
+
+    return st.Page(
+        page,
+        url_path=url_path,
+        title=title,
+        icon=icon,
+        default=url_path == "",
+    )
+
+
+pages = {
+    "AMPL Streamlit Apps": [
+        app_page(home, "🏠", "Home"),
+        app_page(risk_return.main, "📈", "Risk Return"),
+        app_page(tips.main, "💡", "Modeling Tips"),
+    ],
+    "Real applications": [
+        app_page(aircrew_training_scheduling.main, "✈️", "Aircrew Training Scheduling"),
+        app_page(batch_process.main, "⚙️", "Batch Process Optimization"),
+        app_page(facility_location.main, "🏭", "Stochastic Facility Location"),
+    ],
+    "Puzzles & Games": [
+        app_page(bistro_game.main, "🍽️", "Bistro Game"),
+        app_page(global_optimization.main, "🎅", "Global Optimization"),
+        app_page(nqueens.main, "👑", "N-Queens"),
+    ],
+    "Tools & Documentation": [
+        app_page(python.main, "🐍", "Python Integration"),
+        app_page(reformulation_explorer.main, "🔍", "Reformulation Explorer"),
+    ],
+}
+
+pg = st.navigation(pages)
+pg.run()
