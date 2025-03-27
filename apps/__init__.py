ffa66a9dfd7a63376358145f16fc17a763ffe7e3
Filipe Brandão <fdabrandao@gmail.com>
2025-03-27 12:37:06 +0000

    Hide hit counter

--- apps/__init__.py
+++ apps/__init__.py
@@ -3,10 +3,11 @@ import platform
 
 
 def badge(url_path=""):
-    if platform.system() == "Linux":
-        badge_html = f"""<img src="https://h.ampl.com/https://amplopt.streamlit.app/{url_path}">"""
-    else:
-        badge_html = ""
+    # if platform.system() == "Linux":
+    #     badge_html = f"""<img src="https://h.ampl.com/https://amplopt.streamlit.app/{url_path}">"""
+    # else:
+    #     badge_html = ""
+    badge_html = ""
     return f"""
         <div style="float:right;">
         {badge_html}
