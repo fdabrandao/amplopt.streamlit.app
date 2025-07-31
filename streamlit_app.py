8c5d0e8ca8eda6dcf4ebef82f8d4659fcd8cd09f
Filipe Brandão <fdabrandao@gmail.com>
2025-07-31 16:36:43 +0100

    Official url: fix environment variable check again

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__))))
 
 
 def redirect_link():
-    if not os.environ.get("DEPLOYMENT_METHOD", "") != "docker-deployment":
+    if os.environ.get("DEPLOYMENT_METHOD", "") != "docker-deployment":
         return
     components.html(
         """
