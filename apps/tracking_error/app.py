834adfed1e838df0aa9645ba0256aeedbdc1666f
Filipe Brandão <fdabrandao@gmail.com>
2025-10-25 18:23:16 +0100

    Tracking Error: fix 403 forbidden error when retrieving data from wikipedia

--- apps/tracking_error/app.py
+++ apps/tracking_error/app.py
@@ -57,7 +57,18 @@ s.t. Turnover{if sum{i in ASSETS} w_prev[i] > 0}:
 def get_sp500_symbols():
     """Get current S&P 500 constituents from Wikipedia"""
     url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
-    table = pd.read_html(url)[0]
+
+    headers = {
+        "User-Agent": (
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/120.0.0.0 Safari/537.36"
+        )
+    }
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+
+    table = pd.read_html(response.text)[0]
     return table.set_index("Symbol")
 
 
